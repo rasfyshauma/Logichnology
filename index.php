75cd15ea1d8b21a18ea9667c333ce820f6e99fb5
rasfyshauma <55040597+rasfyshauma@users.noreply.github.com>
2021-04-28 20:13:10 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,419 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <!-- basic -->
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <!-- mobile metas -->
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="viewport" content="initial-scale=1, maximum-scale=1">
+      <!-- site metas -->
+      <title>Dashboard </title>
+      <meta name="keywords" content="">
+      <meta name="description" content="">
+      <meta name="author" content="">
+      <!-- bootstrap css -->
+      <link rel="stylesheet" href="css/bootstrap.min.css">
+      <!-- style css -->
+      <link rel="stylesheet" href="css/style.css">
+      <!-- Responsive-->
+      <link rel="stylesheet" href="css/responsive.css">
+      <link rel="stylesheet" href="css/owl.carousel.min.css">
+      <!-- fevicon -->
+      <link rel="icon" href="images/fevicon.png" type="image/gif" />
+      <!-- Scrollbar Custom CSS -->
+      <link rel="stylesheet" href="css/jquery.mCustomScrollbar.min.css">
+      <!-- Tweaks for older IEs-->
+      <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css" media="screen">
+      <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
+   </head>
+   <!-- body -->
+   <body class="main-layout">
+      <!-- loader  -->
+      <div class="loader_bg">
+         <div class="loader"><img src="images/loading.gif" alt="#" /></div>
+      </div>
+      <!-- end loader -->
+      <!-- header -->
+      <header>
+         <!-- header inner -->
+         <div class="header_bg">
+            <div class="header">
+               <div class="container">
+                  <div class="row">
+                     <div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col logo_section">
+                        <div class="full">
+                           <div class="center-desk">
+                              <div class="logo">
+                                 <a href="index.html"></a>
+                              </div>
+                           </div>
+                        </div>
+                     </div>
+                     <div class="col-xl-9 col-lg-9 col-md-9 col-sm-9">
+                        <ul class="costomer">
+                           <li><a href="#"></li>
+                        </ul>
+                     </div>
+                  </div>
+               </div>
+            </div>
+            <!-- end header inner -->
+            <!-- end header -->
+            <!-- banner -->
+            <section class="banner_main">
+               <div class="container-fluid">
+                  <div class="row d_flex">
+                     <div class="col-md-5">
+                        <div class="text-bg">
+                           <span>Jasa<br> Pengetikan Tugas</span>
+                           <p>Mungkin saat ini ada kewalahan dalam mengerjakan tugas anda, kami disini akan melayani anda jika anda memerlukan kami. Silahkan isi nama, email, nomor telepon, dan keluhan anda pada laman bawah. </p>
+                        </div>
+                     </div>
+                     <div class="col-md-7">
+                        <div class="text-img">
+                           <figure><img src="images/img.png" /></figure>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </section>
+         </div>
+      </header>
+      <!-- end banner -->
+      <!-- three_box  -->
+      <div id="service" class="three_box">
+         <div class="container">
+            <div class="row">
+               <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12">
+                  <div class="Soft-box">
+                     <i><img src="images/service1.png" alt="#" /></i>
+                     <h3>Hemat</h3>
+                     <p>Anda dapat menghemat dan membagi waktu anda dengan melakukan hal yang lain.</p>
+                  </div>
+               </div>
+               <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12">
+                  <div class="Soft-box">
+                     <i><img src="images/service2.png" alt="#" /></i>
+                     <h3>Cepat</h3>
+                     <p>Tidak membutuhkan waktu yang lama untuk kami mengerjakan tugas anda.</p>
+                  </div>
+               </div>
+               <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12">
+                  <div class="Soft-box">
+                     <i><img src="images/service3.png" alt="#" /></i>
+                     <h3>Tepat</h3>
+                     <p>Memastikan bahwa apa yang dikerjakan tepat dengan apa yang anda inginkan.</p>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+      <!-- end three_box -->
+      <!-- weare -->
+      <div id="why" class="weare">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-12">
+                  <div class="titlepage">
+                     <h2>Siapakah <span class="white">Tim Kami</span></h2>
+                  </div>
+               </div>
+            </div>
+            <div class="row">
+               <div class="col-md-12">
+                  <div class="main_weare">
+                     <div class="row">
+                        <div class="col-xl-5 col-lg-5 col-md-5 col-sm-12">
+                           <div id="box_ho" class="weare-img_box">
+                              <figure><img src="images/rasfy.jpg" alt="#" /></figure>
+                           </div>
+                        </div>
+                        <div class="col-xl-7 col-lg-7 col-md-7 col-sm-12">
+                           <div class="weare-box">
+                              <h3>Rasfy Shauma Moisa Asda</h3>
+							  <p> 201910370311445 </p> 
+                              <p> Universitas Muhammadiyah Malang </p> 
+							  <p> Teknik Informatika </p> 
+							  <p> Email : rasfyshaumamoisaasda@webmail.umm.ac.id </p>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="main_weare2 ">
+                     <div class="row">
+                        <div class="col-xl-5 col-lg-5 col-md-5 col-sm-12">
+                           <div id="box_ho" class="weare-img_box">
+                              <figure><img src="images/gannys.jpg" alt="#" /></figure>
+                           </div>
+                        </div>
+                        <div class="col-xl-7 col-lg-7 col-md-7 col-sm-12">
+                           <div class="weare-box">
+                             <h3>Gannys Widya Iganingtyas</h3>
+							  <p> 201910370311423 </p> 
+                              <p> Universitas Muhammadiyah Malang </p> 
+							  <p> Teknik Informatika </p> 
+							  <p> Email : widyagannys@webmail.umm.ac.id </p>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+				  <br>
+				   <div class="main_weare2 ">
+                     <div class="row">
+                        <div class="col-xl-5 col-lg-5 col-md-5 col-sm-12">
+                           <div id="box_ho" class="weare-img_box">
+                              <figure><img src="images/septia.jpg" alt="#" /></figure>
+                           </div>
+                        </div>
+                        <div class="col-xl-7 col-lg-7 col-md-7 col-sm-12">
+                           <div class="weare-box">
+                              <h3>Septia Annindita</h3>
+							  <p> 201910370311407 </p> 
+                              <p> Universitas Muhammadiyah Malang </p> 
+							  <p> Teknik Informatika </p> 
+							  <p> Email : septia19@webmail.umm.ac.id </p>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+      <!-- end weare -->
+      <!-- testimonial -->
+      <div class="testimonial">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-12">
+                  <div class="titlepage">
+                     <h2>Testimoni <span class="white">Klien</span></h2>
+                     <p>Berikut testimoni dari beberapa klien kami yang<br>
+                        berlangganan pada Jasa Pengetikan Tugas.
+                     </p>
+                  </div>
+               </div>
+            </div>
+            <section id="demos">
+               <div class="row">
+                  <div class="col-md-12">
+                     <div class="owl-carousel owl-theme">
+                        <div class="item">
+                           <span></span>
+                           <h3>Annonymous </h3>
+                           <p>Web ini sangat membantu sebagai perantara untuk kamu yang membutuhkan jasa ini! </p>
+                        </div>
+                        <div class="item">
+                           <span></span>
+                           <h3>Annonymous </h3>
+                           <p>Terimakasih web jasa pengetikan tugas!! Saya jadi merasa lebih lega.</p>
+                        </div>
+                        <div class="item">
+                           <span></span>
+                           <h3>Budi </h3>
+                           <p>Mengapa saya baru menemukan web ini sekarang!! Ini benar-benar sangat membantu.</p>
+                        </div>
+                        <div class="item">
+                           <span></span>
+                           <h3>Yunta </h3>
+                           <p>Setidaknya kamu yang saat ini sedang menumpuk tugasnya, web ini akan menjadi perantara ke jasa yang akan membantu kamu!</p>
+                        </div>
+                        <div class="item">
+                           <span></span>
+                           <h3>Irnah </h3>
+                           <p>Suka bangett, terimakasihhh jasa pengetikan!!!!</p>
+                        </div>
+                        <div class="item">
+                           <span></span>
+                           <h3>Annonymous </h3>
+                           <p>Keren!</p>
+                        </div>
+                        <div class="item">
+                           <span></span>
+                           <h3>Upin </h3>
+                           <p>Saya suka, saya suka!</p>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </section>
+         </div>
+      </div>
+      <!-- end testimonial -->
+      <!-- Asked -->
+      <div  class="asked_main">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-12">
+                  <div class="titlepage">
+                     <h2>Frequently Asked <span class="white"> Questions</span></h2>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div class="container-fluid">
+            <div class="row">
+               <div class="col-md-6">
+                  <div class="ask_img">
+                     <figure><img src="images/ask.jpg" alt="#"/></figure>
+                  </div>
+               </div>
+               <div class="col-md-6">
+                  <div class="ask_box">
+                     <div class="simpaly">
+                        <div class="slider"><span>+</span><a href="#">
+                           Apakah kita akan dihubungi oleh admin setelah mendaftar?</a>
+                        </div>
+                        <div class="content">
+                           <p> Setelah anda mendaftar, nomor yang anda cantumkan akan dihubungi oleh pihak jasa pengetikan tugas. 
+                           </p>
+                        </div>
+                     </div>
+                     <div class="simpaly">
+                        <div class="slider"><span>+</span><a href="#">
+                           Berapa lama pengerjaan tugas yang diberikan?</a>
+                        </div>
+                        <div class="content">
+                           <p> Tergantung seberapa tingkat kesulitan pada tugas anda tersebut.
+                           </p>
+                        </div>
+                     </div>
+                     <div class="simpaly">
+                        <div class="slider"><span>+</span><a href="#">
+                           Bagaimana jika ada kesalahan dalam tugas yang sudah diberikan?</a>
+                        </div>
+                        <div class="content">
+                           <p> Anda dapat menghubungi admin yang menjadi jasa dalam pengerjaan tugas anda.
+                           </p>
+                        </div>
+                     </div>
+                     <div class="simpaly">
+                        <div class="slider"><span>+</span><a href="#">
+                           Bagaimana jika setelah mendaftar, kita tidak dihubungi oleh admin?</a>
+                        </div>
+                        <div class="content">
+                           <p> Anda dapat konfirmasikan kepada call center yang tertera dan memberitahu mengenai biodata anda.
+                           </p>
+                        </div>
+                     </div>
+                     <div class="simpaly">
+                        <div class="slider"><span>+</span><a href="#">
+                           Berapa biaya yang dikenakan oleh Jasa Pengetikan Tugas?</a>
+                        </div>
+                        <div class="content">
+                           <p> Dimulai dari harga Rp.5.000! Ayo tunggu apalagi?
+                           </p>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+      </div>
+      <!-- end Asked -->
+      <!-- contact testimonial-->
+      <div id="contact" class="contact">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-6">
+                  <div class="titlepage">
+                     <h2>Contact <span class="white">Us</span></h2>
+					 <p>Hubungi Kami : 1234567890</p>
+                  </div>
+                  <div class="cont">
+                     <span>Isilah 
+                     Biodata Anda </br>
+                     Untuk Jasa Pengetikan</span>
+                     <a href="#">Laman Atas</a>
+                  </div>
+               </div>
+               <div class="col-md-6">
+                  <form class="main_form">
+                     <div class="row">
+                        <div class="col-sm-12">
+                           <input class="contactus" placeholder="Full Name" type="text" name="
+                              Full Name">
+                        </div>
+                        <div class="col-sm-12">
+                           <input class="contactus" placeholder="Email" type="text" name=" Email">
+                        </div>
+                        <div class="col-sm-12">
+                           <input class="contactus" placeholder="Phone" type="text" name="Phone">
+                        </div>
+                        <div class="col-sm-12">
+                           <textarea class="textarea" placeholder="Message" type="text" name="Message"></textarea>
+                        </div>
+                        <div class="col-sm-12">
+                           <button class="send">Send</button>
+                        </div>
+                     </div>
+                  </form>
+               </div>
+            </div>
+         </div>
+      </div>
+      <!-- end contact -->
+      <!--  footer -->
+      <footer>
+         <div class="footer">
+            <div class="container">
+               <div class="row">
+                  <div class="col-md-12">
+                     <p>© 2019 All Rights Reserved. <a href="https://html.design/">Logichnology</a></p>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </footer>
+      <!-- end footer -->
+      <!-- Javascript files-->
+      <script src="js/jquery.min.js"></script>
+      <script src="js/popper.min.js"></script>
+      <script src="js/bootstrap.bundle.min.js"></script>
+      <script src="js/jquery-3.0.0.min.js"></script>
+      <script src="js/plugin.js"></script>
+      <!-- sidebar -->
+      <script src="js/jquery.mCustomScrollbar.concat.min.js"></script>
+      <script src="js/custom.js"></script>
+      <script src="js/owl.carousel.min.js"></script>
+      <script src="https:cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.js"></script>
+      <script>
+         $(document).ready(function() {
+           var owl = $('.owl-carousel');
+           owl.owlCarousel({
+             margin: 10,
+             nav: true,
+             loop: true,
+             responsive: {
+               0: {
+                 items: 1
+               },
+               600: {
+                 items: 2
+               },
+               1000: {
+                 items: 3
+               }
+             }
+           })
+         })
+      </script>
+      
+      <script>
+         $('.content').hide();
+         
+         $('.slider').click(function() {
+         $(this).next('.content').slideToggle();
+         this.toggle = !this.toggle;
+         $(this).children("span").text(this.toggle ? "-" : "+");
+         return false;
+         });
+      </script>
+   </body>
+</html>
+
